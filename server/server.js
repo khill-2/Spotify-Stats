b9bb2f862bb03c2e500c9789674af7b55e64e273
keller hill <hillkc@umich.edu>
2025-06-22 19:15:14 +0200

    server bug

--- server/server.js
+++ server/server.js
@@ -38,6 +38,8 @@ app.post('/auth/token', async (req, res) => {
   params.append('code', code);
   params.append('redirect_uri', redirect_uri);
 
+  console.log('Using redirect_uri for token exchange:', redirect_uri);
+
   // params.append('client_id', SPOTIFY_CLIENT_ID);
   // params.append('client_secret', SPOTIFY_CLIENT_SECRET);
   const authHeader = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
