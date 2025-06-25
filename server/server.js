41d2c9c37d35e6fc20915ba5c6707b0aa665f801
keller hill <hillkc@umich.edu>
2025-06-25 22:41:59 +0200

    Supabase new user insertion

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.post('/auth/token', async (req, res) => {
 
   params.append('client_id', SPOTIFY_CLIENT_ID);
   params.append('client_secret', SPOTIFY_CLIENT_SECRET);
-  // const authHeader = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
+  const authHeader = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
 
 
   try {
